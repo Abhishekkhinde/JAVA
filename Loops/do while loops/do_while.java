6a04b83b757e8059056e04bc9e72018ac73908c9
Abhishek Khinde <130373568+Abhishekhinde@users.noreply.github.com>
2023-04-15 12:25:26 +0530

    Add files via upload

---
+++ Loops/do while loops/do_while.java
@@ -0,0 +1,12 @@
+public class do_while {
+    public static void main(String[] args) {
+
+        int num = 1;
+
+        do {
+            System.out.println(num);
+            num++;
+        }
+        while (num<=10);
+    }
+}
