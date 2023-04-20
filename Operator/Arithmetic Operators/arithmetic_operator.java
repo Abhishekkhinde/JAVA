7111c90d541001d4e29f660181d02eb9aa51e22a
Abhishek Khinde <130373568+Abhishekhinde@users.noreply.github.com>
2023-04-20 16:23:54 +0530

    Add files via upload

---
+++ Operator/Arithmetic Operators/arithmetic_operator.java
@@ -0,0 +1,21 @@
+public class arithmetic_operator {
+    public static void main(String args[]){
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a+b); // addition
+        System.out.println(a-b); // subtraction
+        System.out.println(a*b); // multiplication
+        System.out.println(a/b); // Division
+        System.out.println(a%b); // module
+
+
+        System.out.println(a++); // increment
+        System.out.println(++a); // increment
+
+
+        System.out.println(b--); // decrement
+        System.out.println(--b); // decrement
+    }
+}
