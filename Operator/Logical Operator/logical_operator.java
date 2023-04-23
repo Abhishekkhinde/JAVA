ab62db4765fb7312b27c7cd7817f8edaae580c3a
Abhishek Khinde <130373568+Abhishekhinde@users.noreply.github.com>
2023-04-23 14:42:52 +0530

    Update logical_operator.java

--- Operator/Logical Operator/logical_operator.java
+++ Operator/Logical Operator/logical_operator.java
@@ -1,1 +1,20 @@
+public class logical_operator {
+    public static void main(String[] args) {
+        int p = 10;
+        int q = 20;
 
+        //    &&    logical and
+        System.out.println(p<q && p<=q);  // true
+        System.out.println(p<q && p==q);  // false
+        System.out.println(p>q && p==q);  // false
+
+        //    ||    logical or
+        System.out.println(p<q || p<=q);  // true
+        System.out.println(p>q || p==q);  // false
+        System.out.println(p<q || p==q);  // true
+
+        //    !     logical not
+        System.out.println(!(p==q));      // true
+        System.out.println(!(p<q));       // false
+    }
+}
