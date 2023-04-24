03d039dfc1cc58e0c620e91f35614fa4df0b5b2e
Abhishek Khinde <130373568+Abhishekhinde@users.noreply.github.com>
2023-04-24 12:39:48 +0530

    Update and rename relational_operator,java to relational_operator.java

---
+++ Operator/Relational Operator/relational_operator.java
@@ -0,0 +1,15 @@
+public class relational_operator {
+    public static void main(String[] args) {
+        int p = 2;
+        int q = 2;
+        int r = 3;
+        int s = 4;
+
+        System.out.println(p==q);
+        System.out.println(p!=q);
+        System.out.println(p>r);
+        System.out.println(p<r);
+        System.out.println(p>=s);
+        System.out.println(p<=s);
+    }
+}
