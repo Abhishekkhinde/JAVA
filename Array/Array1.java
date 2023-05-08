ab0fa947fe07a004b5dc09b2e9054484dcfcd55b
Abhishek Khinde <130373568+Abhishekhinde@users.noreply.github.com>
2023-05-08 23:08:46 +0530

    Create Array1.java

---
+++ Array/Array1.java
@@ -0,0 +1,32 @@
+public class Array1 {
+    public static void main(String Args[]){
+        int arr1[] = new int[] {10,20,30,40};
+        char arr2[] = new char[] {'a','b','c','d'};
+        float arr3[] = new float[] {3.5f,2.1f,1.7f,6.7f};
+        boolean arr4[] = new boolean[] {true,false,true,false};
+
+        System.out.println("arr1 : integer");
+        System.out.println(arr1[0]);
+        System.out.println(arr1[1]);
+        System.out.println(arr1[2]);
+        System.out.println(arr1[3]);
+
+        System.out.println("arr2 : char");
+        System.out.println(arr2[0]);
+        System.out.println(arr2[1]);
+        System.out.println(arr2[2]);
+        System.out.println(arr2[3]);
+
+        System.out.println("arr3 : float");
+        System.out.println(arr3[0]);
+        System.out.println(arr3[1]);
+        System.out.println(arr3[2]);
+        System.out.println(arr3[3]);
+
+        System.out.println("arr4 : boolean");
+        System.out.println(arr4[0]);
+        System.out.println(arr4[1]);
+        System.out.println(arr4[2]);
+        System.out.println(arr4[3]);
+    }
+}
