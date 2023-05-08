45bb1bf0e74ffb79b682879a290eafdabdf88922
Abhishek Khinde <130373568+Abhishekhinde@users.noreply.github.com>
2023-05-08 23:54:57 +0530

    Create Array_3.java

---
+++ Array/Array_3.java
@@ -0,0 +1,20 @@
+public class Array_3 {
+    public static void main(String[] args) {
+
+        int arr[] = new int[5];
+
+        // for single index
+        arr[0] =15;
+        arr[1] =10;
+        arr[2] =67;
+        arr[3] =45;
+        arr[4] =87;
+
+        // print single index
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+    }
+}
