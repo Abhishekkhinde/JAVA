ac19c983b2c01a6f6392b4e7855eda4f234a518c
Abhishek Khinde <130373568+Abhishekhinde@users.noreply.github.com>
2023-05-20 23:37:54 +0530

    Add files via upload

---
+++ Array/Array_9.java
@@ -0,0 +1,51 @@
+// copy array element to another array
+        // shallow copy
+
+import java.util.Scanner;
+
+public class Array_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter array size");
+        int size = sc.nextInt();
+
+        int[] arr1 = new int[size];
+
+        int[] arr2 ;
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+        }
+        System.out.println();
+        arr2 = arr1;
+        System.out.println("arr2");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr2[i]);
+        }
+
+        System.out.println();
+
+        System.out.println("after changing element");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        arr2[2] = a;
+        arr2[3] = b;
+        System.out.println("array3");
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+        }
+        System.out.println();
+        System.out.println("array2");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr2[i]);
+        }
+    }
+}
+
